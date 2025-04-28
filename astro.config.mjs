fbe3caa9acbe6c1b9136062f0af29b9fa3ef1614
bachdx2812 <bachdx.hut@gmail.com>
2025-04-28 16:11:38 +0700

    fix metadata for blog detail page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import vue from "@astrojs/vue";
 
 export default defineConfig({
+  site: 'https://caoduyhung.vercel.app/',
   integrations: [tailwind(), vue()],
 });
